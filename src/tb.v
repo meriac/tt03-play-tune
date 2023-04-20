1d6a92b1e90ce41c57595809bad51a1ce42de0b0
Milosch Meriac <milosch@meriac.com>
2023-04-20 17:01:01 +0400

    Initial check-in

---
+++ src/tb.v
@@ -0,0 +1,34 @@
+`default_nettype none
+`timescale 1ns/1ps
+
+/*
+this testbench just instantiates the module and makes some convenient wires
+that can be driven / tested by the cocotb test.py
+*/
+
+module tb (
+    // testbench is controlled by test.py
+    input clk,
+    input rst,
+    output [1:0] speaker
+   );
+
+    // this part dumps the trace to a vcd file that can be viewed with GTKWave
+    initial begin
+        $dumpfile ("tb.vcd");
+        $dumpvars (0, tb);
+        #1;
+    end
+
+    // wire up the inputs and outputs
+    wire [7:0] inputs = {6'b0, rst, clk};
+    wire [7:0] outputs;
+    assign speaker = outputs[1:0];
+
+    // instantiate the DUT
+    meriac_tt03_play_tune #(.MAX_COUNT(100)) meriac_tt03_play_tune(
+        .io_in  (inputs),
+        .io_out (outputs)
+        );
+
+endmodule
---
+++ src/test.py
@@ -0,0 +1,16 @@
+import cocotb
+from cocotb.clock import Clock
+from cocotb.triggers import RisingEdge, FallingEdge, Timer, ClockCycles
+
+@cocotb.test()
+async def test_tune(dut):
+    dut._log.info("start")
+    clock = Clock(dut.clk, 320, units="us")
+    cocotb.fork(clock.start())
+
+    dut._log.info("reset")
+    dut.rst.value = 1
+    await ClockCycles(dut.clk, 10)
+    dut.rst.value = 0
+
+    await ClockCycles(dut.clk, 78125)
